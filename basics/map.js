9eca8c29d93f25129b578ffd4252bc536a673230
S48AHan <saberahmedn2@gmail.com>
2026-08-25 11:48:53 +0600

    ➕ Add map
    Type(s): ADD
    
    Issue(s):
    - JIRA: none

--- basics/map.js
+++ basics/map.js
@@ -1,14 +1,23 @@
 const arr = [1, 2, 3, 4];
 
-const doubledArr = arr.map((x) => x * 2);
+// const doubledArr = arr.map((x) => x * 2);
 
-console.log("Mapped Array:",doubledArr);
+// console.log("Mapped Array:",doubledArr);
 
-const filterArr = arr.filter((x) => x % 2 === 0);
+// const filterArr = arr.filter((x) => x % 2 === 0);
 
-console.log("Filtered Array:",filterArr);
+// console.log("Filtered Array:",filterArr);
 
-const reducedArr = arr.reduce((acc,cur)=>{
-    return acc+ cur;
-},0);
-console.log("Rediced Arrat:", reducedArr)
+// const reducedArr = arr.reduce((acc,cur)=>{
+//     return acc+ cur;
+// },0);
+// console.log("Rediced Arrat:", reducedArr)
+
+const oddArr = arr.filter((x) => x % 2 !== 0);
+console.log(oddArr);
+const tripleArr = oddArr.map((x) => x * 3);
+console.log(tripleArr);
+const total = tripleArr.reduce((acc, cur) => {
+  return acc + cur;
+}, 0);
+console.log(total)
